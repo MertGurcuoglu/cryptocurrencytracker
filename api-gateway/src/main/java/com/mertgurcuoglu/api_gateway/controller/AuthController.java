0b5b040e644338443b0683e02ab8bd6ec5916eb0
MertGurcuoglu <mrthkk23@gmail.com>
2025-12-12 15:56:34 +0300

    chrore:trigger ci pipelines manually

--- api-gateway/src/main/java/com/mertgurcuoglu/api_gateway/controller/AuthController.java
+++ api-gateway/src/main/java/com/mertgurcuoglu/api_gateway/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
+//trigger manually
 
 @RestController
 @RequestMapping("/api/auth")
--- eureka-server/src/main/java/com/mertgurcuoglu/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/mertgurcuoglu/eureka_server/EurekaServerApplication.java
@@ -3,6 +3,7 @@ package com.mertgurcuoglu.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+//trigger manually
 
 @SpringBootApplication
 @EnableEurekaServer 
